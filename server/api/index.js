6b12fb671fb1c6f475128a3cb299c53fa7079686
briaar <briarusss@gmail.com>
2024-02-03 21:10:33 -0500

    writing out a guideline for myself

--- server/api/index.js
+++ server/api/index.js
@@ -1,1 +1,18 @@
 // TODO: add API router here and all API sub-routers
+
+const express = require('express');
+const apiRouter = express.Router();
+
+// const { requireUser, requireAdmin } = require('../auth/utils');
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+
+// <------------------------ HOMEPAGE ------------------------>
+
+// <------------------------ FACE SHAPE ------------------------>
+
+// <------------------------ SKIN TONES ------------------------>
+
+module.exports = apiRouter;
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,1 +1,55 @@
 // TODO: seed the DB using Prisma
+const prisma = require("./client");
+
+// <------------------------ HOMEPAGE ------------------------>
+
+
+// <------------------------ FACE SHAPE ------------------------>
+
+
+
+    // <---- DIAMOND ---->
+
+    // <---- HEART ---->
+
+    // <---- ROUND ---->
+
+    // <---- OVAL ---->
+
+    // <---- SQUARE ---->
+        
+    // <---- TRIANGLE ---->
+
+
+
+
+
+
+// <------------------------ SKIN TONE ------------------------>
+
+
+
+    // <---- SKIN TONE: FAIR ---->
+        
+    // <---- SKIN TONE: LIGHT ---->
+        
+    // <---- SKIN TONE: LIGHT MEDIUM  ---->
+        
+    // <---- SKIN TONE: MEDIUM  ---->
+        
+    // <---- SKIN TONE: MEDIUM TAN ---->
+        
+    // <---- SKIN TONE: TAN ---->
+        
+    // <---- SKIN TONE: TAN DARK ---->
+        
+    // <---- SKIN TONE: DARK ---->
+        
+    // <---- SKIN TONE: DARK DEEP ---->
+        
+    // <---- SKIN TONE: DEEP ---->
+
+
+
+
+
