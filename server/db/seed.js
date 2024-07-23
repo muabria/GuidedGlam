192ceb117c6af48f28ee903256a192a2ddeaaa50
briaar <briarusss@gmail.com>
2024-07-23 10:34:21 -0400

    attempting to update and fix

--- server/db/seed.js
+++ server/db/seed.js
@@ -49,6 +49,13 @@ const prisma = require("./client");
         
     // <---- SKIN TONE: DEEP ---->
 
+    seed().then(async () => {
+        await prisma.$disconnect();
+    }).catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect();
+        process.exit(1)
+    })
 
 
 
